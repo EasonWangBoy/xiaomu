4e33192eabb48422f9fb9e3cbdf5d86a08e5afbb
EasonWangBoy <wangshuo926@foxmail.com>
2019-09-04 08:52:10 +0800

    day日考

---
+++ 日考/day4/four/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>{{arr}}</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data(){
+    return {
+      arr:['a','2h','3c','4a']
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  created(){
+    this.arrForeach()
+  },
+  methods:{
+    arrForeach(){
+      this.arr.forEach((item)=>{
+        this.arr+=item
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
